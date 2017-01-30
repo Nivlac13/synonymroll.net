2e391130cd5f055044fe8fcf71215f11190080c4
Calvin Loveland <calvinloveland@gmail.com>
2017-01-30 11:40:53 -0700

    Added css and roll.jpg

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <DOCTYPE=html>
 <html>
 <head>
+	<title>Synonym Roll</title>
+	<link rel="stylesheet"  type="text/css" src="styles.css"/>
 </head>
 <body>
+	<h1>Synonym Roll</h1>
+	<form>
+		<input type="text" name="word"/>
+	</form>
+	<img src="roll.jpg">
 </body>
 </html>
 
