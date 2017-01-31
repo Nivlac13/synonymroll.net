eac67bead401b518457ca50ab5845c62e29a340c
Calvin Loveland <calvinloveland@gmail.com>
2017-01-31 00:47:29 -0700

    PHP

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
 			else{
 				$api_results= unserialize(file_get_contents("https://words.bighugelabs.com/api/2/f20ef01bf13e1ab38ce70b8885a03846/$word/php"));
 				if(empty($api_results["noun"])){
-					$api_results["noun"]=array("syn" => array(""))}
+					$api_results["noun"]=array("syn" => array(""));}
 				if(empty($api_results["verb"])){
-					$api_results["verb"]=array("syn" => array(""))}
+					$api_results["verb"]=array("syn" => array(""));}
 				$synonyms = array_merge($api_results["noun"]["syn"],$api_results["verb"]["syn"]);
 				print_r($synonyms);
 			}
