a96de2e5c1c5fa48696ab1bae3ef52fe092771cb
Calvin Loveland <calvinloveland@gmail.com>
2017-01-30 23:45:46 -0700

    CSS changes and initial php

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
 	<link rel="stylesheet"  type="text/css" href="styles.css"/>
 </head>
 <body>
-	<h1>Synonym Roll</h1>
+	<h1>
+		<?php 
+			if($_GET["word"]==null){
+				echo"Synonym Roll";}
+			else{
+				echo unserialize()$_GET["https://words.bighugelabs.com/api/2/f20ef01bf13e1ab38ce70b8885a03846/" . $_GET["word"]."/php"];}
+	</h1>
 	<form>
 		<input type="text" name="word"/>
 	</form>
