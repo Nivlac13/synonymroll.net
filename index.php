590027dd7b698c2296883a59efa93facfeeb8807
Me 250379 <250379@synonymroll.nfshost.com>
2017-03-18 21:28:51 +0000

    added spinning, change styles, added placeholder

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 			fclose($file);
 			$key = trim($key);
 			if(empty($_GET["word"])){
-				echo"Synonym Roll";}
+				echo"SynonymRoll.net";}
 			else{
 				$word = $_GET["word"];
 				$api_results= unserialize(file_get_contents("http://words.bighugelabs.com/api/2/$key/$word/php"));
@@ -22,17 +22,24 @@
 				if(empty($api_results["verb"])){
 					$api_results["verb"]=array("syn" => array(""));}
 				$synonyms = array_merge($api_results["noun"]["syn"],$api_results["verb"]["syn"]);
+				echo("Some synonyms of ".$word." are: ");
 				foreach($synonyms as &$synonym){
 					if($synonym != ""){
 						echo $synonym.", ";}}	
 			}
 		?>
 	</h1>
-	<form>
-		<input type="text" name="word" class="word-input"/>
+	<form onsubmit="spinTime()">
+		<input type="text" name="word" id="word-input" placeholder="Enter a word"/>
 		<br/>
-		<input type="image" src="roll.jpg" alt="Submit Form" class="submit-image"/>
+		<input type="image" src="roll.jpg"  alt="Submit Form" id="submit-image" class="" />
 	</form>
+	<script>
+		function spinTime() {
+			document.getElementById("submit-image").className += " spinning";
+			document.querySelector("form").submit();
+		}
+	</script>
 </body>
 </html>
 
